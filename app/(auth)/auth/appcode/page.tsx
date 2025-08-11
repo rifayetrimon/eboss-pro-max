f06e18632f915b7c5d16d4d77765889c89da4ce0
rifayet <rifayetrimon88@gmail.com>
2025-08-11 15:58:43 +0800

    trying to solve issue in path

--- app/(auth)/auth/appcode/page.tsx
+++ app/(auth)/auth/appcode/page.tsx
@@ -3,6 +3,7 @@
 import ComponentAppCodeForm from '@/components/(auth)/component-appcode';
 import LanguageDropdown from '@/components/language-dropdown';
 import Alert from '@/components/ui/alert';
+import Image from 'next/image';
 import { useState } from 'react';
 
 const AppCodePage = () => {
@@ -22,14 +23,14 @@ const AppCodePage = () => {
     return (
         <div>
             <div className="absolute inset-0">
-                <img src="/assets/images/auth/coming-soon-object3.png" alt="image" className="h-full w-full object-cover" />
+                <Image src="/assets/images/auth/coming-soon-object3.png" alt="image" className="h-full w-full object-cover" />
             </div>
 
             <div className="relative flex min-h-screen items-center justify-center bg-[url(/assets/images/auth/map.png)] bg-cover bg-center bg-no-repeat px-6 py-10 dark:bg-[#060818] sm:px-16">
-                <img src="/assets/images/auth/coming-soon-object1.png" alt="image" className="absolute left-0 top-1/2 h-full max-h-[893px] -translate-y-1/2" />
-                <img src="/assets/images/auth/coming-soon-object2.png" alt="image" className="absolute left-24 top-0 h-40 md:left-[30%]" />
-                <img src="/assets/images/auth/coming-soon-object3.png" alt="image" className="absolute right-0 top-0 h-[300px]" />
-                <img src="/assets/images/auth/polygon-object.svg" alt="image" className="absolute bottom-0 end-[28%]" />
+                <Image src="/assets/images/auth/coming-soon-object1.png" alt="image" className="absolute left-0 top-1/2 h-full max-h-[893px] -translate-y-1/2" />
+                <Image src="/assets/images/auth/coming-soon-object2.png" alt="image" className="absolute left-24 top-0 h-40 md:left-[30%]" />
+                <Image src="/assets/images/auth/coming-soon-object3.png" alt="image" className="absolute right-0 top-0 h-[300px]" />
+                <Image src="/assets/images/auth/polygon-object.svg" alt="image" className="absolute bottom-0 end-[28%]" />
 
                 <div className="relative w-full max-w-[870px] rounded-md bg-[linear-gradient(45deg,#fff9f9_0%,rgba(255,255,255,0)_25%,rgba(255,255,255,0)_75%,_#fff9f9_100%)] p-2 dark:bg-[linear-gradient(52.22deg,#0E1726_0%,rgba(14,23,38,0)_18.66%,rgba(14,23,38,0)_51.04%,rgba(14,23,38,0)_80.07%,#0E1726_100%)]">
                     <div className="relative flex flex-col justify-center rounded-md bg-white/60 px-6 py-20 backdrop-blur-lg dark:bg-black/50 lg:min-h-[758px]">
--- next.config.js
+++ next.config.js
@@ -1,14 +1,21 @@
 // next.config.js
 const isProd = process.env.NODE_ENV === 'production';
 
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
     reactStrictMode: true,
     basePath: isProd ? '/eboss-pro-max' : '',
+    assetPrefix: isProd ? '/eboss-pro-max' : '',
+    trailingSlash: true,
+    images: {
+        unoptimized: isProd, // Add this if you're doing static export
+    },
     env: {
-        NEXT_PUBLIC_BASE_URL: isProd ? 'https://devphp01.awfatech.com' : 'http://localhost:3000',
+        NEXT_PUBLIC_BASE_URL: isProd ? 'https://devphp01.awfatech.com/eboss-pro-max' : 'http://localhost:3000',
     },
 };
 
+module.exports = nextConfig;
 // /** @type {import('next').NextConfig} */
 // const nextConfig = {
 //     reactStrictMode: true,
