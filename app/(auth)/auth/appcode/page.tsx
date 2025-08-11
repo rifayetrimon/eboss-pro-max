157daefcc311b5000338acc330fc11ec7e30b646
rifayet <rifayetrimon88@gmail.com>
2025-08-11 19:50:09 +0800

    Static file path changed

--- app/(auth)/auth/appcode/page.tsx
+++ app/(auth)/auth/appcode/page.tsx
@@ -30,16 +30,16 @@ const AppCodePage = () => {
             <div
                 className="relative flex min-h-screen items-center justify-center px-6 py-10 dark:bg-[#060818] sm:px-16"
                 style={{
-                    backgroundImage: `url(${assetPath('/auth/assets/images/auth/map.png')})`,
+                    backgroundImage: `url(${assetPath('/assets/images/auth/map.png')})`,
                     backgroundSize: 'cover',
                     backgroundPosition: 'center',
                     backgroundRepeat: 'no-repeat',
                 }}
             >
-                <Image src={assetPath('/auth/assets/images/auth/coming-soon-object1.png')} alt="image" fill className="absolute left-0 top-1/2 -translate-y-1/2 object-contain" />
-                <Image src={assetPath('/auth/assets/images/auth/coming-soon-object2.png')} alt="image" width={160} height={160} className="absolute left-24 top-0 h-40 md:left-[30%]" />
-                <Image src={assetPath('/auth/assets/images/auth/coming-soon-object3.png')} alt="image" width={300} height={300} className="absolute right-0 top-0" />
-                <Image src={assetPath('/auth/assets/images/auth/polygon-object.svg')} alt="image" width={200} height={200} className="absolute bottom-0 end-[28%]" />
+                <Image src={assetPath('/assets/images/auth/coming-soon-object1.png')} alt="image" fill className="absolute left-0 top-1/2 -translate-y-1/2 object-contain" />
+                <Image src={assetPath('/assets/images/auth/coming-soon-object2.png')} alt="image" width={160} height={160} className="absolute left-24 top-0 h-40 md:left-[30%]" />
+                <Image src={assetPath('/assets/images/auth/coming-soon-object3.png')} alt="image" width={300} height={300} className="absolute right-0 top-0" />
+                <Image src={assetPath('/assets/images/auth/polygon-object.svg')} alt="image" width={200} height={200} className="absolute bottom-0 end-[28%]" />
 
                 {/* Content box */}
                 <div className="relative w-full max-w-[870px] rounded-md bg-[linear-gradient(45deg,#fff9f9_0%,rgba(255,255,255,0)_25%,rgba(255,255,255,0)_75%,_#fff9f9_100%)] p-2 dark:bg-[linear-gradient(52.22deg,#0E1726_0%,rgba(14,23,38,0)_18.66%,rgba(14,23,38,0)_51.04%,rgba(14,23,38,0)_80.07%,#0E1726_100%)]">
