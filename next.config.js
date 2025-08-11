3ec2dce39fa9d0d8b437d901b796eb9ae53b265b
rifayet <rifayetrimon88@gmail.com>
2025-08-11 15:19:35 +0800

    update next js config

--- next.config.js
+++ next.config.js
@@ -1,18 +1,24 @@
-// next.config.js
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//     reactStrictMode: true,
+//     eslint: {
+//         ignoreDuringBuilds: true,
+//     },
+//     output: 'standalone',
+//     basePath: '/eboss-pro-max',
+//     assetPrefix: '/eboss-pro-max/',
+// };
+
+// module.exports = nextConfig;
+
+// // next.config.js
 const isProd = process.env.NODE_ENV === 'production';
 
 module.exports = {
     reactStrictMode: true,
-
-    // Set base path and asset prefix only in production
-    basePath: isProd ? '/eboss-pro-max' : '',
-    assetPrefix: isProd ? '/eboss-pro-max/' : '',
-
-    // Optional: If you want images to work with the basePath
-    images: {
-        unoptimized: true, // Disable Image Optimization if using static export
+    env: {
+        BASE_URL: isProd
+            ? 'https://devphp01.awfatech.com/eboss-pro-max' // Production domain
+            : 'http://localhost:3000', // Local dev
     },
-
-    // Optional: If you want to handle trailing slashes consistently
-    trailingSlash: false,
 };
