e9a1cb66ab51b186902808f7846360b691457a65
rifayet <rifayetrimon88@gmail.com>
2025-09-11 16:43:49 +0800

    edit address input field

--- app/(defaults)/staff/staff_two/page.tsx
+++ app/(defaults)/staff/staff_two/page.tsx
@@ -17,11 +17,6 @@ export default function StaffDataTableTwo() {
             </ul>
 
             <div>
-                {/* <div className="panel flex items-center overflow-x-auto whitespace-nowrap p-3 text-primary">
-                    <div className="rounded-full bg-primary p-1.5 text-white ring-2 ring-primary/30 ltr:mr-3 rtl:ml-3">
-                        <IconBell />
-                    </div>
-                </div> */}
                 <h2 className="text-md mt-2 font-bold">Staff Listing</h2>
                 <StaffTableTwo />
             </div>
